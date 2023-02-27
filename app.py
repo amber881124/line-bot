6b9e12bb300e07a189f1ec47d38eee81ea56a2b7
momo <zyongzhu24@gmail.com>
2023-02-27 14:01:47 +0800

    add app.py line bot

--- app.py
+++ app.py
@@ -1,46 +1,68 @@
+from __future__ import unicode_literals
+import os
 from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, StickerSendMessage
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+import configparser
+
+import random
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('pqPyEuP2PgA4CDav5+mPrUJ9qnrjX3VsgBIMJKDkgrg2wR520omyFooJkIL7KgGih10pEqcLojEbjvusYbXtGobgGHH+FTWDNSLA8zyLxMmuAF2MqEN9JYd3cWuW3msG7WPZz7w4+AGRGJDpu89OugdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('d7aafd79765e40d9530d54952306aafa')
+# LINE 聊天機器人的基本資料
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 
 
+# 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
-    # handle webhook body
+    
     try:
+        print(body, signature)
         handler.handle(body, signature)
+        
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
 
-
+# 學你說話
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
+def pretty_echo(event):
+    # 自己加的
+    msg = event.message.text
+    pretty_note = '♫♪♬♡'
+    pretty_text = ''
+    
+    if '貼圖' in msg:
+        sticker_message = StickerSendMessage(
+        package_id='1',
+        sticker_id='1'
+    )
+        line_bot_api.reply_message(
+        event.reply_token,
+        sticker_message
+    )
+        return
+
+    for i in msg:
+        pretty_text += i
+        pretty_text += random.choice(pretty_note)
+    # 原本的
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
-
+        TextSendMessage(text=pretty_text)
+    )
 
 if __name__ == "__main__":
     app.run()
